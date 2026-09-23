780026c95694e6b0c022363dec7b4ecd6ab4c477
pos <pgousdal@users.noreply.github.com>
2026-09-24 01:27:30 +0200

    M0.2: add include/axiomica/types.h

---
+++ include/axiomica/types.h
@@ -0,0 +1,9 @@
+#ifndef AXIOMICA_TYPES_H
+#define AXIOMICA_TYPES_H
+
+typedef unsigned char ax_u8;
+typedef unsigned short ax_u16;
+typedef unsigned int ax_u32;
+typedef signed int ax_s32;
+
+#endif
